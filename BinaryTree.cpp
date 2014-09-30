15c73d97d220669983c7128f9fed5a062ae5222f
cfanrui <efanabe@126.com>
2014-09-30 20:44:32 +0800

    Update BinaryTree.cpp

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -133,9 +133,63 @@ void BinaryTree::LevelOrder(std::string& output) const
 }
 void BinaryTree::SLevelOrder(std::string& output) const
 {
-
+	output.clear();
+	if (!m_pTree) return;
+	bool isOdd = true;
+	size_t levelNodeCnt = 1;
+	std::queue<TreeNode*> que;
+	que.push(m_pTree);
+	while (!que.empty())
+	{
+		size_t nextLevelCnt = 0;
+		if (isOdd)
+		{
+			std::stack<TreeNode*> s;
+			while (levelNodeCnt--)
+			{
+				TreeNode* p = que.front();
+				que.pop();
+				s.push(p);
+				if (p->m_pLeft)
+				{
+					nextLevelCnt++;
+					que.push(p->m_pLeft);
+				}
+				if (p->m_pRight)
+				{
+					nextLevelCnt++;
+					que.push(p->m_pRight);
+				}
+			}
+			while (!s.empty())
+			{
+				output.push_back(s.top()->m_cValue);
+				s.pop();
+			}
+		}
+		else
+		{
+			while (levelNodeCnt--)
+			{
+				TreeNode* p = que.front();
+				que.pop();
+				output.push_back(p->m_cValue);
+				if (p->m_pLeft)
+				{
+					nextLevelCnt++;
+					que.push(p->m_pLeft);
+				}
+				if (p->m_pRight)
+				{
+					nextLevelCnt++;
+					que.push(p->m_pRight);
+				}
+			}
+		}
+		levelNodeCnt = nextLevelCnt;
+		isOdd = !isOdd;
+	}
 }
-
 void LevelAt(TreeNode* T, size_t level, std::string& output)
 {
 	if (NULL != T)
@@ -157,20 +211,75 @@ void BinaryTree::LevelAt(size_t level, std::string& output) const
 	if (level<1 || level>Height() || NULL==m_pTree) return;
 	::LevelAt(m_pTree, level, output);
 }
+size_t Height(TreeNode* T)
+{
+	if (!T) return 0;
+	size_t left = Height(T->m_pLeft);
+	size_t right = Height(T->m_pRight);
+	if (left > right)
+		return left + 1;
+	else return right + 1;
+}
 size_t BinaryTree::Height() const
 {
-	if (m_nNodeCount > 0)
-		return (size_t)(log10(double(m_nNodeCount))
-			/ log10(double(2))) + 1;
-	else return 0;
+	return ::Height(m_pTree);
 }
 size_t BinaryTree::Width() const
 {
-	return 0;
+	std::string levels;
+	size_t height = Height();
+	size_t maxWidth = 0;
+	if (m_pTree)
+	{
+		for (size_t level = 1; level <= height; level++)
+		{
+			levels.clear();
+			this->LevelAt(level, levels);
+			if (maxWidth < levels.size()) maxWidth = levels.size();
+		}
+	}
+	return maxWidth;
 }
-size_t BinaryTree::LongestPath(std::string&) const
+//calculate the longest path length
+void LongestPath(TreeNode* T, size_t& max)
 {
-	return 0;
+	static size_t pathLen = 0;
+	if (T)
+	{
+		pathLen++;
+		if (T->m_pLeft == NULL && T->m_pRight == NULL && 
+			pathLen > max) max = pathLen;
+		LongestPath(T->m_pLeft, max);
+		LongestPath(T->m_pRight, max);
+		pathLen--;
+	}
+}
+void LongestPath(TreeNode* T, const size_t& max, std::vector<std::string>& output)
+{
+	static size_t pathLen = 0;
+	static std::string outStr;
+	if (T)
+	{
+		pathLen++;
+		outStr.push_back(T->m_cValue);
+		if (T->m_pLeft == NULL && T->m_pRight == NULL &&
+			pathLen == max)
+		{
+			output.push_back(outStr);
+		}
+		LongestPath(T->m_pLeft, max, output);
+		LongestPath(T->m_pRight, max, output);
+		pathLen--;
+		outStr.pop_back();
+	}
+}
+size_t BinaryTree::LongestPath(std::vector<std::string>& output) const
+{
+	output.clear();
+	size_t longest = 0;
+	::LongestPath(m_pTree, longest);
+	::LongestPath(m_pTree, longest, output);
+	return longest;
 }
 size_t BinaryTree::NumberOfNodes() const
 {
